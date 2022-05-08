7d13955e258005a542a4cef2b4995e4ca36fd34b
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-05-08 22:05:54 +0300

    Lisätty HTML-sivuille Accordion, grid ja muut layoutit

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 });
+
+var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+var collapseList = collapseElementList.map(function (collapseEl) {
+  return new bootstrap.Collapse(collapseEl) 
+})
+
+var myCollapse = document.getElementById('myCollapse')
+var bsCollapse = new bootstrap.Collapse(myCollapse, {
+  toggle: false
+})
+
+var myCollapsible = document.getElementById('myCollapsible')
+myCollapsible.addEventListener('hidden.bs.collapse', function () {
+  // do something...
+})
