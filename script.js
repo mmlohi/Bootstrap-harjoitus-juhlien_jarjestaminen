2523abf7c03979ed179ef311c44efad5d10355e1
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-05-09 18:34:07 +0300

    Lisätty linkkejä buttoneihin ja muuta sisältöä

--- script.js
+++ script.js
@@ -4,22 +4,3 @@ $(document).ready(function () {
     });
 });
 
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-var collapseList = collapseElementList.map(function (collapseEl) {
-  return new bootstrap.Collapse(collapseEl) 
-})
-
-var myCollapse = document.getElementById('myCollapse')
-var bsCollapse = new bootstrap.Collapse(myCollapse, {
-  toggle: false
-})
-
-var myCollapsible = document.getElementById('myCollapsible')
-myCollapsible.addEventListener('hidden.bs.collapse', function () {
-  // do something...
-})
-
-openNewWindow = function()
-{
- window.open(url, "_blank");
-}
