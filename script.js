ad2d08d14cb454f016792cfaf2fd81c3889b31a6
Minna  Lohi <101732406+mmlohi@users.noreply.github.com>
2022-05-14 10:09:24 +0300

    Lisätty kuvia, eventtejä, selektoreita ja manipulointia

--- script.js
+++ script.js
@@ -4,3 +4,55 @@ $(document).ready(function () {
         $('#sidebar').toggleClass('active');
     });
 });
+
+const element = document.getElementById("myBtn");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+
+var x = document.getElementById("myDIV");
+
+/*addEventListener() method to attach an "animationstart",
+ "animationiteration" and "animationend" event*/
+
+ // Start the animation with JavaScript
+function myFunction() {
+    x.style.WebkitAnimation = "mymove 4s 2"; // Code for Chrome, Safari and Opera
+    x.style.animation = "mymove 4s 2";     // Standard syntax
+  }
+
+ // Code for Chrome, Safari and Opera
+x.addEventListener("webkitAnimationStart", myStartFunction);
+x.addEventListener("webkitAnimationIteration", myRepeatFunction);
+x.addEventListener("webkitAnimationEnd", myEndFunction);
+
+// Standard syntax
+x.addEventListener("animationstart", myStartFunction);
+x.addEventListener("animationiteration", myRepeatFunction);
+x.addEventListener("animationend", myEndFunction);
+
+function myStartFunction() {
+  this.innerHTML = "Sait rakkaimman ihmisen rinnallesi kulkemaan";
+  this.style.backgroundColor = "pink";
+}
+
+function myRepeatFunction() {
+  this.innerHTML = "animationiteration event occured - The animation was played again";
+  this.style.backgroundColor = "lightgreen";
+}
+
+function myEndFunction() {
+  this.innerHTML = "animationend event occured - The animation has completed";
+  this.style.backgroundColor = "lightgray";
+}
+
+document.getElementById("menubutton").addEventListener("click", showMenu);
+let menu = document.querySelector("#menubutton div");
+
+function showMenu(event){
+    event.preventDefault();
+    menu.classList.toggle("invisible");
+
+}
--- yesno.js
+++ yesno.js
@@ -1,29 +1,86 @@
-new Vue({ 
-    el: '#app',
-    vuetify: new Vuetify(),
-    data: {
-        image: { url: ""}
-    },
-    created(){
-        this.loadNextImage();
-    } ,
-    methods:{
-        async loadNextImage()
-        {
-            try{
-                axios.defaults.headers.common['x-api-key'] = "DEMO-API-KEY" // Replace this with your API Key
+// JokeAPI
 
-                let response = await axios.get('https://api.thecatapi.com/v1/images/search', { params: { limit:1, size:"full" } } ) // Ask for 1 Image, at full resolution
-                
-                this.image = response.data[0] // the response is an Array, so just use the first item as the Image
+console.log(Object.values(JokeAPI));
 
-                console.log("-- Image from TheCatAPI.com")
-                console.log("id:", this.image.id)
-                console.log("url:", this.image.url)
-
-            }catch(err){
-                console.log(err)
-            }
-        }
-    }
+JokeAPI.getJokes({
+  jokeType: "single"
 })
+  .then((r) => r.json())
+  .then((data) => {
+    updateUI(data);
+  });
+
+// To update the joke on the UI
+function updateUI(jokeData) {
+  const $ = (id) => document.getElementById(id);
+
+  $("joke--text").innerHTML = jokeData.joke;
+}
+
+
+const element = document.getElementById("myBtn");
+element.addEventListener("click", myFunction1);
+
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "Katso Amor-veikkoa, kun löysi kaksi heikkoa.";
+}
+
+var x = document.getElementById("myDIV");
+
+
+/*addEventListener() method to attach an "animationstart",
+ "animationiteration" and "animationend" event*/
+// Start the animation with JavaScript
+function myFunction() {
+  x.style.WebkitAnimation = "mymove 4s 2"; // Code for Chrome, Safari and Opera
+  x.style.animation = "mymove 4s 2";     // Standard syntax
+}
+
+// Code for Chrome, Safari and Opera
+x.addEventListener("webkitAnimationStart", myStartFunction);
+x.addEventListener("webkitAnimationIteration", myRepeatFunction);
+x.addEventListener("webkitAnimationEnd", myEndFunction);
+
+// Standard syntax
+x.addEventListener("animationstart", myStartFunction);
+x.addEventListener("animationiteration", myRepeatFunction);
+x.addEventListener("animationend", myEndFunction);
+
+function myStartFunction() {
+  this.innerHTML = "animationstart event occured - The animation has started";
+  this.style.backgroundColor = "lightpink";
+}
+
+function myRepeatFunction() {
+  this.innerHTML = "animationiteration event occured - The animation was played again";
+  this.style.backgroundColor = "green";
+}
+
+function myEndFunction() {
+  this.innerHTML = "animationend event occured - The animation has completed";
+  this.style.backgroundColor = "lightgray";
+}
+
+document.getElementById("menubutton").addEventListener("click", showMenu);
+let menu = document.querySelector("#menubutton div");
+
+function showMenu(event){
+    event.preventDefault();
+    menu.classList.toggle("invisible");
+
+}
+
+function insertElement() {
+    // Creating a new div element 
+    var newDiv = document.createElement("div");
+     
+    // Creating a text node 
+    var newContent = document.createTextNode("Hi, how are you doing?");
+     
+    // Adding the text node to the newly created div
+    newDiv.appendChild(newContent);
+     
+    // Adding the newly created element and its content into the DOM 
+    var currentDiv = document.getElementById("main"); 
+    document.body.appendChild(newDiv, currentDiv);
+}	
